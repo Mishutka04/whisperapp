4a8430c1958bf12d52d304410584b767c909dd3f
Mishutka04 <vitalykodolov@yandex.ru>
2024-12-15 16:31:51 +0300

    init

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,21 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
 from src.swagger.router import router as router_swagger
-app = FastAPI()
+app = FastAPI(
+        root_path="/api",
+        docs_url="/"
+    )
 
 
+origins = ['*']
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router_swagger)
 
